9dbdf67d906384b96adc7c33d50315e2a9bc8fd1
alexey glotov <alexey.glotov@imaginarity.com>
2026-06-05 16:00:24 +0200

    backend also gets base url from .env

--- backend/src/main/java/com/ecommerce/buyme/security/config/WebConfig.java
+++ backend/src/main/java/com/ecommerce/buyme/security/config/WebConfig.java
@@ -2,11 +2,16 @@ package com.ecommerce.buyme.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
+    @Value("${BASE_URL}")
+    private String baseUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -17,7 +22,7 @@ public class WebConfig {
                 //         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 
                 registry.addMapping("/api/**")
-                    .allowedOrigins("http://localhost:3000")
+                    .allowedOrigins(baseUrl)
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .exposedHeaders("*")
